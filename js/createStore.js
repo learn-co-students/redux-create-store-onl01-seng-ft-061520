9810921775220d97885187a9f797e3d7c6302281
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-10-30 12:30:11 -0700

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,4 +1,20 @@
-let state;
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState,
+  };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,27 +26,13 @@ function reducer(state = { count: 0 }, action) {
   }
 }
 
-function createStore() {
-  let state;
-
-  function dispatch(action) {
-    state = reducer(state, action);
-    render();
-  }
-  function getState() {
-    return state;
-  }
-  return { dispatch, getState };
-}
-
 function render() {
   let container = document.getElementById("container");
   container.textContent = store.getState().count;
 }
 
-let store = createStore();
+let store = createStore(reducer); // createStore takes the reducer as an argument
 store.dispatch({ type: "@@INIT" });
-
 let button = document.getElementById("button");
 
 button.addEventListener("click", () => {
